1674a309d9115117364cb988d0491c826077213d
nekkox <108139374+nekkox@users.noreply.github.com>
2023-03-11 20:05:00 +0100

    x

--- Courier.php
+++ Courier.php
@@ -2,5 +2,19 @@
 
 class Courier
 {
+    public $name;
+    public $home_country;
+    public function __construct($name) {
+        $this->name = $name;
+    }
+    public function ship($parcel = null) {
 
+        if(isset($parcel)){
+            $this->home_country = $parcel;
+            return true;
+        }else {
+            $this->home_country = "Nie podanu adresu";
+            return false;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php
+require_once ("Courier.php");
+$courier = new Courier("DHL");
+$isShipped = $courier->ship("dsdasdad");
+var_dump($courier);
+var_dump($isShipped);
